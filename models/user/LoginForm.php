1f9ba94042a734fbc8548ed17efce92db515a044
Kirill Aleksandrov <xakepgame@gmail.com>
2016-07-27 15:46:15 +0700

    add email validation

--- models/user/LoginForm.php
+++ models/user/LoginForm.php
@@ -26,11 +26,9 @@ class LoginForm extends Model
     public function rules()
     {
         return [
-            // username and password are both required
             [['email', 'password'], 'required'],
-            // rememberMe must be a boolean value
+            ['email', 'email'],
             ['rememberMe', 'boolean'],
-            // password is validated by validatePassword()
             ['password', 'validatePassword'],
         ];
     }
