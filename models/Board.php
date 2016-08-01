2726c2fefc639e2fae7dd036b51e39a4c9ccbc36
Kirill Aleksandrov <xakepgame@gmail.com>
2016-08-01 23:39:05 +0700

    move piece array

--- models/Board.php
+++ models/Board.php
@@ -13,6 +13,46 @@ use yii\base\Model;
 
 class Board extends Model
 {
+    const PIECES = [
+        //white pieces
+        'rw1' => ['default_x' => 1, 'default_y' => 1],
+        'nw1' => ['default_x' => 2, 'default_y' => 1],
+        'bw1' => ['default_x' => 3, 'default_y' => 1],
+        'kw1' => ['default_x' => 4, 'default_y' => 1],
+        'qw1' => ['default_x' => 5, 'default_y' => 1],
+        'bw2' => ['default_x' => 6, 'default_y' => 1],
+        'nw2' => ['default_x' => 7, 'default_y' => 1],
+        'rw2' => ['default_x' => 8, 'default_y' => 1],
+
+        'pw1' => ['default_x' => 1, 'default_y' => 2],
+        'pw2' => ['default_x' => 2, 'default_y' => 2],
+        'pw3' => ['default_x' => 3, 'default_y' => 2],
+        'pw4' => ['default_x' => 4, 'default_y' => 2],
+        'pw5' => ['default_x' => 5, 'default_y' => 2],
+        'pw6' => ['default_x' => 6, 'default_y' => 2],
+        'pw7' => ['default_x' => 7, 'default_y' => 2],
+        'pw8' => ['default_x' => 8, 'default_y' => 2],
+
+        //black pieces
+        'rb1' => ['default_x' => 1, 'default_y' => 8],
+        'nb1' => ['default_x' => 2, 'default_y' => 8],
+        'bb1' => ['default_x' => 3, 'default_y' => 8],
+        'kb1' => ['default_x' => 4, 'default_y' => 8],
+        'qb1' => ['default_x' => 5, 'default_y' => 8],
+        'bb2' => ['default_x' => 6, 'default_y' => 8],
+        'nb2' => ['default_x' => 7, 'default_y' => 8],
+        'rb2' => ['default_x' => 8, 'default_y' => 8],
+
+        'pb1' => ['default_x' => 1, 'default_y' => 7],
+        'pb2' => ['default_x' => 2, 'default_y' => 7],
+        'pb3' => ['default_x' => 3, 'default_y' => 7],
+        'pb4' => ['default_x' => 4, 'default_y' => 7],
+        'pb5' => ['default_x' => 5, 'default_y' => 7],
+        'pb6' => ['default_x' => 6, 'default_y' => 7],
+        'pb7' => ['default_x' => 7, 'default_y' => 7],
+        'pb8' => ['default_x' => 8, 'default_y' => 7],
+    ];
+
     public static function getPieces($game_id)
     {
         $game = Game::findOne($game_id);
@@ -21,43 +61,43 @@ class Board extends Model
         }
 
         $pieces = [];
-        foreach (Piece::PIECES as $key => $item) {
-            $lastTime = GameAction::find()
+        foreach (self::PIECES as $key => $item) {
+            $lastMove = GameAction::find()
                 ->where(['game_id' => $game_id, 'piece_id' => $key])
                 ->orderBy('id DESC')
                 ->limit(1)
                 ->one();
 
             $captured = false;
-            if ($lastTime && GameAction::find()
+            if ($lastMove && GameAction::find()
                     ->where(['game_id' => $game_id])
-                    ->andWhere(['>', 'id', $lastTime->id])
+                    ->andWhere(['>', 'id', $lastMove->id])
+                    ->andWhere(['to_x' => $lastMove->to_x])
+                    ->andWhere(['to_y' => $lastMove->to_y])
                     ->one()) {
 
                 $captured = true;
-            } else if (!$lastTime && GameAction::find()
+            } else if (!$lastMove && GameAction::find()
                     ->where(['game_id' => $game_id])
-                    ->andWhere(['to_x' => Piece::PIECES[$key]['default_x']])
-                    ->andWhere(['to_y' => Piece::PIECES[$key]['default_y']])
+                    ->andWhere(['to_x' => self::PIECES[$key]['default_x']])
+                    ->andWhere(['to_y' => self::PIECES[$key]['default_y']])
                     ->one()) {
 
                 $captured = true;
             }
 
             if ($captured) { }
-            else if (!$lastTime) {
+            else if (!$lastMove) {
                 array_push($pieces, [
                     'id' => $key,
-                    'x'  => Piece::PIECES[$key]['default_x'],
-                    'y'  => Piece::PIECES[$key]['default_y'],
-                    'isSelected' => false,
+                    'x'  => self::PIECES[$key]['default_x'],
+                    'y'  => self::PIECES[$key]['default_y'],
                 ]);
             } else {
                 array_push($pieces, [
                     'id' => $key,
-                    'x'  => $lastTime->to_x,
-                    'y'  => $lastTime->to_y,
-                    'isSelected' => false,
+                    'x'  => $lastMove->to_x,
+                    'y'  => $lastMove->to_y,
                 ]);
             }
         }
--- models/pieces/Piece.php
+++ models/pieces/Piece.php
@@ -6,43 +6,8 @@ use yii\base\Model;
 
 abstract class Piece extends Model
 {
-    const PIECES = [
-        //white pieces
-        'rw1' => ['default_x' => 1, 'default_y' => 1],
-        'nw1' => ['default_x' => 2, 'default_y' => 1],
-        'bw1' => ['default_x' => 3, 'default_y' => 1],
-        'kw1' => ['default_x' => 4, 'default_y' => 1],
-        'qw1' => ['default_x' => 5, 'default_y' => 1],
-        'bw2' => ['default_x' => 6, 'default_y' => 1],
-        'nw2' => ['default_x' => 7, 'default_y' => 1],
-        'rw2' => ['default_x' => 8, 'default_y' => 1],
-
-        'pw1' => ['default_x' => 1, 'default_y' => 2],
-        'pw2' => ['default_x' => 2, 'default_y' => 2],
-        'pw3' => ['default_x' => 3, 'default_y' => 2],
-        'pw4' => ['default_x' => 4, 'default_y' => 2],
-        'pw5' => ['default_x' => 5, 'default_y' => 2],
-        'pw6' => ['default_x' => 6, 'default_y' => 2],
-        'pw7' => ['default_x' => 7, 'default_y' => 2],
-        'pw8' => ['default_x' => 8, 'default_y' => 2],
-
-        //black pieces
-        'rb1' => ['default_x' => 1, 'default_y' => 8],
-        'nb1' => ['default_x' => 2, 'default_y' => 8],
-        'bb1' => ['default_x' => 3, 'default_y' => 8],
-        'kb1' => ['default_x' => 4, 'default_y' => 8],
-        'qb1' => ['default_x' => 5, 'default_y' => 8],
-        'bb2' => ['default_x' => 6, 'default_y' => 8],
-        'nb2' => ['default_x' => 7, 'default_y' => 8],
-        'rb2' => ['default_x' => 8, 'default_y' => 8],
-
-        'pb1' => ['default_x' => 1, 'default_y' => 7],
-        'pb2' => ['default_x' => 2, 'default_y' => 7],
-        'pb3' => ['default_x' => 3, 'default_y' => 7],
-        'pb4' => ['default_x' => 4, 'default_y' => 7],
-        'pb5' => ['default_x' => 5, 'default_y' => 7],
-        'pb6' => ['default_x' => 6, 'default_y' => 7],
-        'pb7' => ['default_x' => 7, 'default_y' => 7],
-        'pb8' => ['default_x' => 8, 'default_y' => 7],
-    ];
+    public static function onBoard($x, $y)
+    {
+        return (1 <= $x) && ($x <= 8) && (1 <= $y) && ($y <= 8);
+    }
 }
