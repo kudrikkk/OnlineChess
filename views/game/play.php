1d8569a802cf9c484832ae7e1bc4128fa790d35f
denis dubovitskiy <dubovitskyden@gmail.com>
2016-08-02 17:53:59 +0700

    add board view

--- views/game/play.php
+++ views/game/play.php
@@ -1,3 +1,5 @@
+<link type="text/css" rel="stylesheet" href="tableForBoard.css"/>
+
 <?php
 
 /** @var \app\models\Game $game */
@@ -24,3 +26,33 @@ use app\models\Board;
 <p>
 <pre><?= print_r(Board::getBoard($game->id)) ?></pre>
 </p>
+<?php $board = Board::getBoard($game->id)?>
+<div class="boardpart">
+    <table border="1px" >
+        <?php for ($i = 0; $i < 8; $i++) : ?>
+            <tr>
+                <?php for ($j = 0; $j < 8; $j++) : ?>
+                    <?php $id = $i*10 + $j; ?> // id для поля доски
+                    <?php /*$name = $board; */?>
+                    <?php if (($i + $j) % 2) : ?>
+                        <?php  ?>
+                        <td class="blackcell">
+                            <?php
+                                echo $board[8 - $i][8 -$j]['piece_id'];
+                            ?>
+                        </td>
+                    <?php else : ?>
+                        <td class="whitecell">
+                            <?php
+                                echo $board[8 - $i][8 - $j]['piece_id'];
+                            ?>
+                        </td>
+                    <?php endif ?>
+                <?php endfor ?>
+            </tr>
+        <?php endfor ?>
+    </table>
+</div>
+<div class="historypart">
+asdfasdfasfsadf
+</div>
